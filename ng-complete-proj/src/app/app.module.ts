843fb739d863b77be528e94d4e8b2a9787f005d2
sahoosni <snigdhas@mindfiresolutions.com>
2020-05-14 16:41:40 +0530

    recipe and shopping layout structures

--- ng-complete-proj/src/app/app.module.ts
+++ ng-complete-proj/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReciepsComponent } from './recieps/recieps.component';
+import { RecipeListComponent } from './recieps/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recieps/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recieps/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReciepsComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng-complete-proj/src/app/recieps/recieps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recieps',
+  templateUrl: './recieps.component.html',
+  styleUrls: ['./recieps.component.css']
+})
+export class ReciepsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
